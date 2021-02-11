3f264a7ec5e3e2a0a25e6321861faa3173df8f22
emna7 <bhmemna7@gmail.com>
2021-02-11 12:44:09 +0100

    task 10

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,7 +1,31 @@
-import * as subject from './subjects/Subject';
-export const cpp;
-export const java;
-export const react;
-export const cTeacher = {
-  experienceTeachingC: 10,
+const cpp = new Subjects.Cpp();
+const java = new Subjects.Java();
+const react = new Subjects.React();
+
+let cTeacher = {
+	firstName: 'Pablo',
+	lastName: 'Escobar',
+	experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
+export {
+	cpp,
+	java,
+	react,
+	cTeacher,
 }
