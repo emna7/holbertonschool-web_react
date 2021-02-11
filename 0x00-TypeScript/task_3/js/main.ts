911f90b3a13f05b486873f688edf9ffd2f374905
emna7 <bhmemna7@gmail.com>
2021-02-11 12:31:32 +0100

    fix

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -5,11 +5,11 @@ import * as CRUD from './crud';
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
-}
+};
 
 const newRowID: RowID = CRUD.insertRow(row);
 console.log(newRowID);
-const updatedRow: RowElement = {'age': 23, ...row};
+const updatedRow: RowElement = { age: 23, ...row };
 CRUD.updateRow(newRowID, updatedRow);
 console.log(updatedRow);
 CRUD.deleteRow(newRowID);
