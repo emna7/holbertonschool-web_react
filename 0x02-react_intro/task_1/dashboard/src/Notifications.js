914a83b831d8c5572c2be3f4b8642d4afb5cee60
emna7 <bhmemna7@gmail.com>
2021-02-17 23:28:09 +0100

    create

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notifications.css';
+
+export const Notification = () => {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+};
+
+export default Notification;
