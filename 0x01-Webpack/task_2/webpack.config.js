0996c877667117ec1e8720886438342b90ac28ba
emna7 <bhmemna7@gmail.com>
2021-03-04 13:24:47 +0100

    create

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename : 'bundle.js'
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["css-loader", "style-loader"],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					"file-loader",
+					{
+						loader: "image-webpack-loader",
+						options: {
+								bypassingOnDebug: true,
+								disable: true,
+						},
+					},
+				],
+			},
+		],
+	},
+};
