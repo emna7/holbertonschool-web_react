3cde74068c89850dadd8d12d36decfdfeb4aee8b
emna7 <bhmemna7@gmail.com>
2021-04-28 23:07:14 +0100

    create

---
+++ 0x07-React_Immutable/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
